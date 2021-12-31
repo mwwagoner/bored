c9ad87833b603e1c395e3fd44cf63d1086459f69
Michael Wagoner <mwwagoner@gmail.com>
2021-12-31 16:33:06 -0500

    Adds mongoose db connection

--- bored-be/src/index.js
+++ bored-be/src/index.js
@@ -1,9 +1,18 @@
 import express from "express";
 import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import { MongoTopologyClosedError } from "mongodb";
 
 // const express = require('express');
 const app = express();
 const port = 8000;
+
+// Mongo connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/bored', {
+    useNewURLParser: true,
+    useUnifiedTopology: true
+})
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
