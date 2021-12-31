9da8f601d258264c7eee661445689ec91252ca34
Michael Wagoner <mwwagoner@gmail.com>
2021-12-31 16:17:42 -0500

    Sets up the bored database model

--- bored-be/src/models/boredModel.js
+++ bored-be/src/models/boredModel.js
@@ -1,1 +1,16 @@
 import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const boredSchema = new Schema({
+    title: String,
+    author: String,
+    publisher: String,
+    minPlayers: Number,
+    maxPlayers: Number,
+    minPlayTime: Number,
+    maxPlayTime: Number,
+    playerAges: Number,
+    numberOfPlays: Number,
+    comments: [{body: String, date: Date}]
+});
