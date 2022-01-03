e2aaf52961e0f137fac50adcfbfe88f2fa201c9b
Michael Wagoner <mwwagoner@gmail.com>
2022-01-03 17:59:52 -0500

    Adds the getGames route

--- bored-be/src/routes/boredRoutes.js
+++ bored-be/src/routes/boredRoutes.js
@@ -1,10 +1,13 @@
-import { addNewGame, getGameById } from "../controllers/boredControllers";
+import { addNewGame, getGameById, getGames } from "../controllers/boredControllers";
 
 const routes = (app) => {
     app.route('/api/addGame')
         .post(addNewGame)
     app.route('/api/game/:gameId')
         .get(getGameById)
+        .put(updateGame)
+    app.route('/api/games')
+        .get(getGames)
 }
 
 export default routes;
