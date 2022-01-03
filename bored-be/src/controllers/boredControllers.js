21226c6d9e9728e353d78312dd65b4052b392e73
Michael Wagoner <mwwagoner@gmail.com>
2022-01-02 23:05:23 -0500

    Adds the addGame controller

--- bored-be/src/controllers/boredControllers.js
+++ bored-be/src/controllers/boredControllers.js
@@ -1,1 +1,15 @@
 import mongoose from "mongoose";
+import { boredSchema } from "../models/boredModel";
+
+const Bored = mongoose.model('Bored', boredSchema);
+
+export const addNewGame = (req, res) => {
+    let newGame = new Bored(req.body);
+
+    newGame.save((err, Bored) => {
+        if (err) {
+            res.send(err)
+        };
+        res.json(Bored)
+    })
+};
