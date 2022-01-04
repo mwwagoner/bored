d2ef965b17f4ee84138cdd8b52e3301db4871670
Michael Wagoner <mwwagoner@gmail.com>
2022-01-04 11:43:28 -0500

    Sets up NavBar component and adds it to the home page

---
+++ bored-fe/src/components/NavBar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export const NavBar = () => {
+    return(
+        <nav>
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+            </ul>
+        </nav>
+    )
+}
