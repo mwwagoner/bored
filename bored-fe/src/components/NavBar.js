0f0027e8a4600028a80a9db1a879c74c4b912bcf
Michael Wagoner <mwwagoner@gmail.com>
2022-01-04 11:56:34 -0500

    Adds placeholder link in the nav bar for the GamesListPage

--- bored-fe/src/components/NavBar.js
+++ bored-fe/src/components/NavBar.js
@@ -8,6 +8,9 @@ export const NavBar = () => {
                 <li>
                     <Link to="/">Home</Link>
                 </li>
+                <li>
+                    <Link to="#">Games</Link>
+                </li>
             </ul>
         </nav>
     )
