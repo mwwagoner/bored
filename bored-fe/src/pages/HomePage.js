3187c47c97904a1d75f2997b4cc293893b6c0aec
Michael Wagoner <mwwagoner@gmail.com>
2022-01-04 10:50:18 -0500

    Creates the home page

---
+++ bored-fe/src/pages/HomePage.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from "react";
+import ListGames from "../components/ListGames"
+
+const HomePage = () => {
+    return(
+        <>
+            <h1>Bored</h1>
+            <p>I don't know exactly how this site will be used but here it is anyway. Ultimately it's going to track my board games.</p>
+        </>
+        
+    )
+};
+
+export default HomePage;
