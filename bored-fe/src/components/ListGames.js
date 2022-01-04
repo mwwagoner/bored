7192b46f4238587ba3af1fa7d2d220176d22d552
Michael Wagoner <mwwagoner@gmail.com>
2022-01-04 14:00:04 -0500

    Changes list of games to a <ul>

--- bored-fe/src/components/ListGames.js
+++ bored-fe/src/components/ListGames.js
@@ -3,14 +3,14 @@ import React from "react";
 const ListGames = ({ gamesList }) => {
     const gamesListArray = gamesList;
     return(
-        <>
+        <ul>
             {gamesListArray.map((game) => {
                 let title = game.title
                 return(
-                    <p>{title}</p>
+                    <li>{title}</li>
                 )
             })}
-        </>
+        </ul>
     )
 }
 
