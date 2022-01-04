987be9afc76da487b42457df5a86037308b9f009
Michael Wagoner <mwwagoner@gmail.com>
2022-01-04 13:37:36 -0500

    Creates the ListGames component and call from the GamesListPage

--- bored-fe/src/components/ListGames.js
+++ bored-fe/src/components/ListGames.js
@@ -1,8 +1,15 @@
 import React from "react";
 
-const ListGames = () => {
+const ListGames = ({ gamesList }) => {
+    const gamesListArray = gamesList;
     return(
         <>
+            {gamesListArray.map((game) => {
+                let title = game.title
+                return(
+                    <p>{title}</p>
+                )
+            })}
         </>
     )
 }
--- bored-fe/src/pages/GamesListPage.js
+++ bored-fe/src/pages/GamesListPage.js
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import ListGames from "../components/ListGames";
 
 const GamesListPage = () => {
+    const [gamesList, setGamesList] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:8000/api/games')
+            .then( (response) => {
+                setGamesList(response.data)
+            })
+    }, []);
+
     return(
         <>
+            <h1>All the best games</h1>
+            <ListGames gamesList={gamesList} />
         </>
     )
 }
