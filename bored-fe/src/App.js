6a5f89b046ceb5675a027de10d8fea0c8f76a9b3
Michael Wagoner <mwwagoner@gmail.com>
2022-01-04 11:58:14 -0500

    Moves the NavBar from HomePage to the App component

--- bored-fe/src/App.js
+++ bored-fe/src/App.js
@@ -6,11 +6,13 @@ import {
 } from "react-router-dom";
 import './App.css';
 import HomePage from './pages/HomePage';
+import { NavBar } from "./components/NavBar";
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <NavBar />
         <Routes>
           <Route path="/" element={<HomePage />} exact />
         </Routes>
--- bored-fe/src/pages/HomePage.js
+++ bored-fe/src/pages/HomePage.js
@@ -5,7 +5,6 @@ import { NavBar } from "../components/NavBar";
 const HomePage = () => {
     return(
         <>
-            <NavBar />
             <h1>Bored</h1>
             <p>I don't know exactly how this site will be used but here it is anyway. Ultimately it's going to track my board games.</p>
         </>
