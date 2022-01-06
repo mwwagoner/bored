d1b4367f2259338149e466ab4929f58d37166541
Michael Wagoner <mwwagoner@gmail.com>
2022-01-06 11:33:16 -0500

    Sets up the page and grabs the URL paramter to test

--- bored-fe/src/pages/GameInfoPage.js
+++ bored-fe/src/pages/GameInfoPage.js
@@ -1,11 +1,17 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import ShowGameInfo from "../components/ShowGameInfo";
+import { useParams } from "react-router-dom";
 
 const GameInfoPage = () => {
+    // Grabs the URL parameter set up in the React Router link to this page
+    // This is the object { gameId: URL Paramter } and is assigned to the const gameId
+    const gameId = useParams()
+
     return(
         <>
-        
+            <h1>Game</h1>
+            <h2>Title: {gameId.gameId}</h2>
         </>
     )
 }
